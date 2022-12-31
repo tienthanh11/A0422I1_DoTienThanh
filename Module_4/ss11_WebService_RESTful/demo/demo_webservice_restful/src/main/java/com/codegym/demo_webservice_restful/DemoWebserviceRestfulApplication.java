859f49e9bd1a134882589aff36c9ce3b80d7b3ec
tienthanh11 <100422533+tienthanh11@users.noreply.github.com>
2022-12-31 16:56:13 +0700

    module 4

---
+++ Module_4/ss11_WebService_RESTful/demo/demo_webservice_restful/src/main/java/com/codegym/demo_webservice_restful/DemoWebserviceRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.demo_webservice_restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebserviceRestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebserviceRestfulApplication.class, args);
+    }
+
+}
---
+++ Module_4/ss11_WebService_RESTful/demo/demo_webservice_restful/src/test/java/com/codegym/demo_webservice_restful/DemoWebserviceRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.demo_webservice_restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebserviceRestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
