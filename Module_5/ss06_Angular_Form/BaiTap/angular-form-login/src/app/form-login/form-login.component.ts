0418edcd61dbaae5973623a2898bae5e21d5fb53
tienthanh11 <100422533+tienthanh11@users.noreply.github.com>
2023-02-28 19:50:05 +0700

    module 5

---
+++ Module_5/ss06_Angular_Form/BaiTap/angular-form-login/src/app/form-login/form-login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-form-login',
+  templateUrl: './form-login.component.html',
+  styleUrls: ['./form-login.component.css']
+})
+export class FormLoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+}
